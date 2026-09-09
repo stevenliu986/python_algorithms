c432bfad20f56a801a9608b31a5d9b43a47855f1
StevenLiu <stevenliu986@gmail.com>
2026-09-09 17:28:39 +1000

    更新代码

--- chapter03/queue.py
+++ chapter03/queue.py
@@ -22,7 +22,7 @@ def hot_potato(name_list,num):
     for name in name_list:
         sim_queue.enqueue(name)
     while sim_queue.size() > 1:
-        for i in range(num):
+        for _ in range(num):
             sim_queue.enqueue(sim_queue.dequeue())
         sim_queue.dequeue()
 
